ea0dbed1200e8d28ee96d3d06c19d65899e8124a
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2021-12-23 00:33:14 -0300

    Funções Privadas e públicas

--- Módulo 1/Capítulo 8.sol
+++ Módulo 1/Capítulo 8.sol
@@ -12,7 +12,7 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string _name, uint _dna) private {
+    function _createZombie(string _name, uint _dna) private { // Mudança da função public para private, com isso precisamos adicionar o _
         zombies.push(Zombie(_name, _dna));
     }
 }

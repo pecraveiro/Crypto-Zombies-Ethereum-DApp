e65cb19fb26b8b8b6d7dbed4ce321e4cfa5d59af
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-02-07 20:23:04 -0300

    Update Capítulo 012.sol

--- Módulo 1/Capítulo 012.sol
+++ Módulo 1/Capítulo 012.sol
@@ -1,1 +1,32 @@
-O site está caindo
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+
+    // declare o nosso evento aqui
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        // dispare o evento aqui
+    } 
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}

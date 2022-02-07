90a66616d0d80b8c76b9655ce0c76826ad199946
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-02-07 20:37:09 -0300

    Capítulo 2: Contratos

--- Módulo 1/Capítulo 001.sol
+++ Módulo 1/Capítulo 001.sol
@@ -3,3 +3,6 @@ pragma solidity ^0.4.19; // Versão do compilador
 contract ZombieFactory { // Criação do contrato
 
 }
+
+// Versão Pragma
+// Todo código fonte em Solidity deve começar com um "version pragma" - uma declaração da versão do compilador Solidity que deve ser usado. Isto é para evitar problemas com futuras versões do compilador, potencialmente apresentando mudanças que possam quebrar o seu código.
